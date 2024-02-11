0ca777913918b4aab51938b0ec9b704442480242
Myles Henehan <mylesjhenehan98@gmail.com>
2024-02-11 21:41:51 +0000

    checking

---
+++ week03/lab3.1.X-testTypes.py
@@ -0,0 +1,15 @@
+# testTypes
+# tests different variables to see what type they are
+# Author: Myles Henehan
+
+i = 12
+fl = 12.02
+isa = False
+phrase = "happy birthday"
+months = ['January', 'February', 'March']
+
+print('variable {} is of type: {} and value: {}'.format('i', type(i), i))
+print('variable {} is of type: {} and value: {}'.format('fl', type(fl), fl))
+print('variable {} is of type: {} and value: {}'.format('isa', type(isa), isa))
+print('variable {} is of type: {} and value: {}'.format('phrase', type(phrase), phrase))
+print('variable {} is of type: {} and value: {}'.format('months', type(months), months))
