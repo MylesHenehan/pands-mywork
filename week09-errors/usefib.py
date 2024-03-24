86426a410f251bedd9895f55c11a902d39060746
Myles Henehan <mylesjhenehan98@gmail.com>
2024-03-24 16:27:03 +0000

    Don't really understand this lab, but I'll come back to it at a later date

---
+++ week09-errors/usefib.py
@@ -0,0 +1,6 @@
+# program that uses myfunctions.py module to prompt a user for a number and return the list of the Fibonacci sequence
+
+import myfunctions 
+
+nTimes = int(input('how many:')) 
+print(myfunctions.fibonacci(nTimes)) 
